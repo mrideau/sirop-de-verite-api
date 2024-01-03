a4267392ee341bb6ae35f52b815d0c6cba1db42d
mrideau <matis.rideau@hotmail.com>
2024-01-03 19:50:49 +0100

    feat: Card serializer

--- cards/serializers.py
+++ cards/serializers.py
@@ -4,13 +4,6 @@ from cards.models import Card
 
 
 class CardSerializer(serializers.ModelSerializer):
-    flags = serializers.MultipleChoiceField(choices=Card.CardFlag)
-
     class Meta:
         model = Card
-        fields = ['id', 'name', 'slug', 'deck', 'flags']
-        lookup_field = 'slug'
-        extra_kwargs = {
-            'url': {'lookup_field': 'slug'},
-            'deck': {'write_only': True}
-        }
+        fields = ['id', 'name', 'content', 'deck', 'no_alcohol']
