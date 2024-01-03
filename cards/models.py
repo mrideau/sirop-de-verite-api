3ca6f71e458958a0910b9b924e3e35199d904229
mrideau <matis.rideau@hotmail.com>
2024-01-03 19:51:02 +0100

    feat: Card model

--- cards/models.py
+++ cards/models.py
@@ -6,13 +6,10 @@ from decks.models import Deck
 
 
 class Card(models.Model):
-    class CardFlag(models.TextChoices):
-        ALCOHOL = "AL", _("Alcohol Related")
-
     name = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100, unique=True)
-    deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
-    flags = MultiSelectField(choices=CardFlag.choices, blank=True, max_length=3)
+    content = models.CharField(max_length=100)
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE, related_name='cards')
+    no_alcohol = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
