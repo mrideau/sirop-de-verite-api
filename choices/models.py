da6c36d0b0053fcf4132b8f233e3189825c86593
mrideau <matis.rideau@hotmail.com>
2024-01-03 19:51:08 +0100

    feat: Choice model

--- choices/models.py
+++ choices/models.py
@@ -4,6 +4,5 @@ from cards.models import Card
 
 
 class Choice(models.Model):
-    selected_card = models.ForeignKey(Card, on_delete=models.CASCADE, related_name='+')
-    other_card = models.ForeignKey(Card, on_delete=models.CASCADE, related_name='+')
-    created_at = models.DateTimeField()
+    card = models.ForeignKey(Card, on_delete=models.CASCADE, related_name='choices')
+    created_at = models.DateTimeField(auto_now_add=True)
